f4c4c840bf1e93b5c56dfc14597666d135c85a64
Phil Bolduc <philbolduc@gmail.com>
2020-01-21 12:07:44 -0800

    Add API skeleton which generates OpenAPI specification

---
+++ api/src/BcGov.Malt.Web/Models/DetailedUser.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BcGov.Malt.Web.Models
+{
+    /// <summary>
+    /// Represents a user with the currently assigned projects
+    /// </summary>
+    public class DetailedUser : User
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyName("projects")]
+        public string[] Projects { get; set; }
+
+    }
+}
---
+++ api/src/BcGov.Malt.Web/Models/Project.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BcGov.Malt.Web.Models
+{
+    /// <summary>
+    /// Represents a project that a user can be granted access to.
+    /// </summary>
+    public class Project
+    {
+        /// <summary>
+        /// The id of the project
+        /// </summary>
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The name of the project
+        /// </summary>
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The type of project, ie Dynamics or SharePoint
+        /// </summary>
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+    }
+}
