b83f08c736d3c0954a9b2b6fa4c63f07b0541e13
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-01-22 14:55:51 -0800

    add in basic root app.js page

---
+++ maltd-frontend/src/App.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>MALTD Frontend</h1>
+    </div>
+  );
+}
