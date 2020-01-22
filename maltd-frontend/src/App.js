f8616ed5fa6dbacd91ba4d4dc3eefb310dda8662
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-01-22 14:06:08 -0800

    cleanup

--- maltd-frontend/src/App.js
+++ maltd-frontend/src/App.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import './App.css';
-import InputField from './components/InputField/InputField';
 
 function App() {
-  const inputField = {
-    name: 'myfield',
-    type: 'text',
-    placeholder: 'hey',
-    valid: true,
-    value: 'hehehe'
-  };
-
   return (
-    <div className="App">
-      
-    </div>
+    <div className="App"></div>
   );
 }
 
