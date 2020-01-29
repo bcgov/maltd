9cceabb55a376917feb7e5c749de299827736fcc
nanyangpro <nanyangpro@gmail.com>
2020-01-28 16:47:32 -0800

    Fixed Linting.

--- maltd-frontend/src/components/composite/UserSearch/UserSearch.test.js
+++ maltd-frontend/src/components/composite/UserSearch/UserSearch.test.js
@@ -2,8 +2,7 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import Adapter from "enzyme-adapter-react-16";
-import Enzyme, { mount } from "enzyme";
-import { Spinner } from "reactstrap";
+import Enzyme from "enzyme";
 import UserSearch from "./UserSearch";
 
 Enzyme.configure({ adapter: new Adapter() });
