11f0432d40b3bb2236f7fe9fca370394c934123f
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-02-06 16:41:26 -0800

    support keyboard users

--- maltd-frontend/src/components/base/BackIcon/BackIcon.js
+++ maltd-frontend/src/components/base/BackIcon/BackIcon.js
@@ -1,11 +1,18 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from "react";
+import PropTypes from "prop-types";
 import { FaAngleLeft } from "react-icons/fa";
 import "./BackIcon.css";
 
 export default function BackIcon({ backIcon: { message }, onClick }) {
   return (
-    <div id="main" onClick={onClick}>
+    <div
+      id="main"
+      role="button"
+      tabIndex="0"
+      onClick={onClick}
+      onKeyDown={onClick}
+    >
       <FaAngleLeft className="size-large" />
       <div className="limit-width">
         <h5>Back</h5>
@@ -14,3 +21,10 @@ export default function BackIcon({ backIcon: { message }, onClick }) {
     </div>
   );
 }
+
+BackIcon.propTypes = {
+  backIcon: PropTypes.shape({
+    message: PropTypes.string.isRequired
+  }).isRequired,
+  onClick: PropTypes.func.isRequired
+};
