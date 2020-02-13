2ac0f566eed94978d63fb4bed619c4fa46f8b458
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-02-12 22:10:15 -0800

    add tests

--- maltd-frontend/src/components/base/Dropdown/Dropdown.stories.js
+++ maltd-frontend/src/components/base/Dropdown/Dropdown.stories.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 import "bootstrap/dist/css/bootstrap.css";
 import Dropdown from "./Dropdown";
 
 const dropdown = {
   defaultTitle: "Choose a project",
-  items: ["System1", "System2"]
+  items: [
+    {
+      id: "123",
+      name: "System 1",
+      type: "Dynamics"
+    },
+    {
+      id: "321",
+      name: "System 2",
+      type: "Sharepoint"
+    }
+  ]
 };
 
 storiesOf("Dropdown", module).add("default", () => (
-  <Dropdown dropdown={dropdown} />
+  <Dropdown dropdown={dropdown} onDropdownClick={action("dropdown clicked")} />
 ));
