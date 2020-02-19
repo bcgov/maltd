e5d9c943c348c214cade3b38f44ef31b3e2f7bc0
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-02-19 12:00:33 -0800

    put back tests now that cypress is running

--- maltd-frontend/cypress/support/pageObjects/landing-page.js
+++ maltd-frontend/cypress/support/pageObjects/landing-page.js
@@ -17,7 +17,7 @@ class LandingPage {
 
   // Get the error text element
   static getErrorText() {
-    return cy.get(".error-message");
+    return cy.get(".small.error-message");
   }
 
   // Get the loading text element
