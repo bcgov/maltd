d2b779eba64fad31120ad55907155fd4626f1ca2
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-02-21 14:30:03 -0800

    add tests to list element

--- maltd-frontend/src/components/base/ListElement/ListElement.test.js
+++ maltd-frontend/src/components/base/ListElement/ListElement.test.js
@@ -1,6 +1,11 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import Adapter from "enzyme-adapter-react-16";
+import Enzyme, { shallow } from "enzyme";
 import ListElement from "./ListElement";
+import XIcon from "../XIcon/XIcon";
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe("List Element", () => {
   test("Component renders as expected", () => {
@@ -15,5 +20,45 @@ describe("List Element", () => {
 
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  test("Component displays XIcon when it should", () => {
+    const testState = { isClicked: false };
+    const onClick = () => {
+      testState.isClicked = true;
+    };
+
+    const wrapper = shallow(
+      <ListElement
+        listElement={{
+          title: "title",
+          description: "description",
+          id: "12"
+        }}
+        onXClick={onClick}
+      />
+    );
+
+    expect(wrapper.contains(<XIcon id="12" onClick={onClick} />)).toBe(true);
+  });
+
+  test("Component does not display XIcon when it should not", () => {
+    const testState = { isClicked: false };
+    const onClick = () => {
+      testState.isClicked = true;
+    };
+
+    const wrapper = shallow(
+      <ListElement
+        listElement={{
+          title: "title",
+          description: "description",
+          id: "12"
+        }}
+        onXClick={null}
+      />
+    );
+
+    expect(wrapper.contains(<XIcon id="12" onClick={onClick} />)).toBe(false);
   });
 });
