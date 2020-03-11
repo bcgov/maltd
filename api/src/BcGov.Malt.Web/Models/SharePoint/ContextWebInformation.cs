cd7aaa164eb09a19c2d0045fcf5c2d6eee9fff54
Phil Bolduc <philbolduc@gmail.com>
2020-03-11 11:42:18 -0700

    Fix class to file layout

---
+++ api/src/BcGov.Malt.Web/Models/SharePoint/ContextWebInformation.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BcGov.Malt.Web.Models.SharePoint
+{
+    public class ContextWebInformation
+    {
+        [JsonPropertyName("FormDigestTimeoutSeconds")]
+        public int FormDigestTimeoutSeconds { get; set; }
+
+        [JsonPropertyName("FormDigestValue")]
+        public string FormDigestValue { get; set; }
+    }
+}
