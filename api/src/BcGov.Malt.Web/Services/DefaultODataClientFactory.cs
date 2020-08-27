c47fbc32f9d15a759bc321920fadbc48f27e6658
Phil Bolduc <philbolduc@gmail.com>
2020-08-27 14:48:23 -0700

    trace OData requests and responses

--- api/src/BcGov.Malt.Web/Services/DefaultODataClientFactory.cs
+++ api/src/BcGov.Malt.Web/Services/DefaultODataClientFactory.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.Logging;
 using Simple.OData.Client;
 
 namespace BcGov.Malt.Web.Services
@@ -6,23 +8,27 @@ namespace BcGov.Malt.Web.Services
     public class DefaultODataClientFactory : IODataClientFactory
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger<ODataClient> _odataClientLogger;
 
-        public DefaultODataClientFactory(IHttpClientFactory httpClientFactory)
+        public DefaultODataClientFactory(IHttpClientFactory httpClientFactory, ILogger<ODataClient> odataClientLogger)
         {
             _httpClientFactory = httpClientFactory ?? throw new System.ArgumentNullException(nameof(httpClientFactory));
+            _odataClientLogger = odataClientLogger ?? throw new ArgumentNullException(nameof(odataClientLogger));
         }
 
         public IODataClient Create(string name)
         {
             if (name is null)
             {
-                throw new System.ArgumentNullException(nameof(name));
+                throw new ArgumentNullException(nameof(name));
             }
 
             HttpClient httpClient = _httpClientFactory.CreateClient(name);
 
             ODataClientSettings settings = new ODataClientSettings(httpClient);
-            settings.UseAbsoluteReferenceUris = false;
+
+            settings.TraceFilter = ODataTrace.All;
+            settings.OnTrace = (message, args) => _odataClientLogger.LogInformation(message, args);
 
             ODataClient oDataClient = new ODataClient(settings);
 
