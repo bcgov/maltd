91731c0e3c963299bea8b08d605227034dad45ea
Phil Bolduc <philbolduc@gmail.com>
2020-09-14 17:31:32 -0700

    Adjust trying to set front end timeout

--- maltd-frontend/src/index.js
+++ maltd-frontend/src/index.js
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-
-if (window.REACT_APP_MALTD_API) {
-  axios.defaults.baseURL = window.REACT_APP_MALTD_API;
-} else if (process.env.REACT_APP_MALTD_API) {
-  axios.defaults.baseURL = process.env.REACT_APP_MALTD_API;
-}
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
